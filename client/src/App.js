d72cf52efcfb25a506ff53c8b5d78270e7c13948
Inge Mationschek <ischek@protonmail.com>
2021-02-18 19:30:22 +0100

    Fixed typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Fibonacci Calculator</h1>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +23,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Route exact path="/" component={Fib} />
+        <Route path="/otherpage" component={OtherPage} />
+      </div>
     </div>
+    </Router>
   );
 }
 
